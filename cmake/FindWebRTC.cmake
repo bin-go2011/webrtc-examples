6fe29c717c55b1563efdb3a466f34bccc1085b36
Ben Ji <bin.go2011@yahoo.com>
2019-08-22 12:18:18 +0800

    FindWebRTC.cmake

--- cmake/FindWebRTC.cmake
+++ cmake/FindWebRTC.cmake
@@ -1,3 +1,30 @@
+
+# ===================================================================================
+#  The WebRTC CMake configuration file
+#
+#  Usage from an external project:
+#    In your CMakeLists.txt, add these lines:
+#
+#    FIND_PACKAGE(WebRTC REQUIRED)
+#    TARGET_LINK_LIBRARIES(MY_TARGET_NAME ${WEBRTC_LIBS})
+#
+#
+#    This file will define the following variables:
+#      - WEBRTC_LIBS                     : The list of libraries to link against.
+#      - WEBRTC_INCLUDE_DIRS             : The WEBRTC include directories.
+#
+# ===================================================================================
+if(NOT WebRTC_ROOT)
+  set(WebRTC_ROOT "$ENV{WebRTC_ROOT}")
+endif()
+
+if(NOT WebRTC_ROOT)
+  find_path(_WebRTC_ROOT NAMES webrtc.gni)
+else()
+  set(_WebRTC_ROOT "${WebRTC_ROOT}")
+endif()
+
+message(STATUS WebRTC_ROOT ${WebRTC_ROOT})
 # WebRTC:: targets
 if(WEBRTC_FOUND)
     add_library(WebRTC::WebRTC INTERFACE IMPORTED)
